f6c8dac1f0b9c4cd7b772e26c5bcd8cbbe90d747
Marwa <marwa.niaaj@gmail.com>
2023-11-29 17:29:44 +0400

    Initial Commit

---
+++ AuthLogin/AuthLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthLoginApp.swift
+//  AuthLogin
+//
+//  Created by Marwa Abou Niaaj on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthLogin
+//
+//  Created by Marwa Abou Niaaj on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
