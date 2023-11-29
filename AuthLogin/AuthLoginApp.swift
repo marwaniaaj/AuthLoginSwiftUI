69b80cce1f210767502e644550505e287fe8c92b
Marwa <marwa.niaaj@gmail.com>
2023-11-29 20:06:37 +0400

    Add Firebase SDK.

--- AuthLogin/AuthLoginApp.swift
+++ AuthLogin/AuthLoginApp.swift
@@ -5,10 +5,26 @@
 //  Created by Marwa Abou Niaaj on 29/11/2023.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
 
 @main
 struct AuthLoginApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
